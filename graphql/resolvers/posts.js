efc6cf3133dbd320ea6220aed6d5e4da0013cd18
Ryan Whittingham <whittingham777@gmail.com>
2020-12-10 11:41:12 -0800

    update blog post function

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -9,6 +9,24 @@ module.exports = {
             } catch (err) {
                 throw new Error(err);
             }
+        },
+        async getPost(_, { postId }){
+            try {
+                const post = await Post.findById(postId);
+                if (post) {
+                    return post;
+                } else {
+                    throw new Error('Post not found')
+                } 
+            }catch (err) {
+                throw new Error(err);    
+            } 
+            
+        }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+
         }
     }
-}
+};
